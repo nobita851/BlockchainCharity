b5f30e9f8fb5f70d37c4b459baa3f5dec912e282
nobita851 <54791621+nobita851@users.noreply.github.com>
2021-04-24 17:19:23 +0530

    Update deploy.js
    Updated deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -12,17 +12,17 @@ const compiledFactory = require('./build/:CampaignFactory.json');
 // }
 const provider = new HDWalletProvider(
 	"answer alien ethics client trophy certain sunny property veteran gown salad poet",
-    "HTTP://127.0.0.1:7545"
+    "https://rinkeby.infura.io/v3/0417fceb8b29486daf83372d30b429e8"
 );
 
 const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log('Attemping to deploy to accounts ', accounts[0]);
+    console.log('Attemping to deploy from account ', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: '0x' + compiledFactory.bytecode })
-        .send({ from: accounts[0] });
+        .send({ gas:'1000000', from: accounts[0] });
 
     console.log('Contract deploy to ', result.options.address);
 };
